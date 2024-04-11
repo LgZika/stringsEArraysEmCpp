fa6b0d702cf12621db72306abd0c5d91fd355b12
Luiz Gustavo Dias Pulz <162385594+LgZika@users.noreply.github.com>
2024-04-11 23:35:00 +0000

    update

---
+++ tamanhoString.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string str = "Hello World";
+    cout << "Tamanho da String: " << str.size() << endl;
+return false;
+}
+
+alias enviaGit="git add . && git commit -m 'update' && git push"

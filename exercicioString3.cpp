7d9cb19ee7d4256ce4863fce26c717ddd470728d
LgZika <luizgustavodiaspulzoficial@gmail.com>
2024-04-15 17:57:25 -0300

    update

--- exercicioString3.cpp
+++ exercicioString3.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(){
     string word;
-    int i = 0;
+    
     cout << "Entre com uma palavra: ";
     cin >> word;
 
-    for(i;i<word.size();i++){
-        if(word[i]=='a'){
-            word[i] = 'a';
+    for(int i = 0; i < word.size() ; i++){ // loop do tamanho da palavra inserida para percorrer a string
+        if(word[i] == 'a'){ // verifica se a letra na posição atual é igual a letra 'a'
+            //cout << word[i] << endl;
+            word[i] = 'e'; // a posição atual recebe a letra 'e' se a letra for 'a'
+        } else if(word[i] == 'A'){
+            word[i] = 'E';
         }
-        cout << i << endl;
     }
-    cout << word << endl;
+    cout << "Palavra modificada: " + word << endl;
 return false;
 }
